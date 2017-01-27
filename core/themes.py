e74586e4d0f249d5e5eaa8c9ed32b6bce23d4da0
Ihor Oleksandrov <ihodev@gmail.com>
2017-01-27 08:04:43 +0200

    fix: remove theme addons from the list of installed themes

--- core/themes.py
+++ core/themes.py
@@ -3,6 +3,8 @@ import re
 import sublime
 import sublime_plugin
 
+from collections import OrderedDict
+
 from ..common import settings
 from ..common.templates.theme import TEMPLATE as THEMETPL
 from ..common.utils import path
@@ -62,22 +64,38 @@ def get_current():
     return current
 
 
-def get_installed():
-    log("Getting installed themes")
+def get_installed(logging=True):
+    if logging:
+        log("Getting installed themes")
 
     theme_resources = sublime.find_resources("*.sublime-theme")
+    all_themes_ordered = OrderedDict([])
     installed_themes = {}
+
+    for res in theme_resources:
+        package = re.sub(PATTERN, "", res)
+        all_themes_ordered[package] = []
 
     for res in theme_resources:
         package = re.sub(PATTERN, "", res)
         theme = os.path.basename(res)
 
-        installed_themes.setdefault(package, []).append(theme)
+        all_themes_ordered[package].append(theme)
 
-    if settings.OVERLAY_ROOT in installed_themes:
-        del installed_themes[settings.OVERLAY_ROOT]
+    for k in all_themes_ordered.keys():
+        value = all_themes_ordered[k]
+        is_addon = False
+        is_patch = True if k == settings.OVERLAY_ROOT else False
 
-    dump(installed_themes)
+        for v in installed_themes.values():
+            if set(value).issubset(set(v)):
+                is_addon = True
+
+        if not (is_addon or is_patch):
+            installed_themes[k] = value
+
+    if logging:
+        dump(installed_themes)
 
     return installed_themes
 
@@ -85,15 +103,19 @@ def get_installed():
 def get_customizable():
     log("Getting the list of theme packages with customization support")
 
+    installed_themes = get_installed(logging=False)
     customizable_themes = []
 
+    # TODO: Clean up in 3.1.0
     prev_res = sublime.find_resources(".st-file-icons")
     curr_res = sublime.find_resources(".supports-a-file-icon-customization")
-
     theme_res = prev_res + curr_res
 
     for res in theme_res:
-        customizable_themes.append(re.sub(PATTERN, "", res))
+        pkg = re.sub(PATTERN, "", res)
+
+        if pkg in installed_themes:
+            customizable_themes.append(pkg)
 
     dump(customizable_themes)
 
@@ -127,15 +149,18 @@ class AfiPatchThemesCommand(sublime_plugin.ApplicationCommand):
                 old = os.path.join(general_old, theme)
                 new = os.path.join(general_new, theme)
 
-                if pkg not in customizable_themes or force_mode:
+                if pkg in customizable_themes and not force_mode:
+                    if os.path.exists(old):
+                        patches_to_clean.append(old)
+
+                    if os.path.exists(new):
+                        patches_to_clean.append(new)
+                else:
                     if not os.path.exists(new) or overwrite:
                         general_to_patch.append(theme)
 
                     if os.path.exists(old):
                         patches_to_clean.append(old)
-                else:
-                    if os.path.exists(new) and not force_mode:
-                        patches_to_clean.append(new)
 
         _clean_patches(patches_to_clean)
 
