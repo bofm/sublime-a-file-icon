3c1ae2aed0d86b50783763f80fcccecaa64ebc84
Ihor Oleksandrov <ihodev@gmail.com>
2017-02-14 12:42:06 +0200

    style: add new lines

--- A File Icon.py
+++ A File Icon.py
@@ -3,6 +3,7 @@ import sublime
 if int(sublime.version()) > 3113:
     from .common import settings
     from .common.utils import cleaning
+
     from .core import icons
     from .core import aliases
 
@@ -13,6 +14,7 @@ if int(sublime.version()) > 3113:
     from .common.utils.reloader import AfiReloadCommand
     from .common.utils.cleaning import AfiCleanCommand
     from .common.utils.cleaning import AfiRevertCommand
+
     from .core.aliases import AfiCheckAliasesCommand
     from .core.themes import AfiPatchThemesCommand
 
