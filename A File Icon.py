d22b8ad8463133ef703da4502a9e8ba5b19a8ce5
Ihor Oleksandrov <ihodev@gmail.com>
2017-03-07 15:02:57 +0200

    fix: fix plugin crash on manual install

--- A File Icon.py
+++ A File Icon.py
@@ -3,11 +3,11 @@ import sublime
 if int(sublime.version()) > 3113:
     from .common import settings
     from .common.utils import cleaning
+    from .common.utils import logging
 
     from .core import icons
     from .core import aliases
 
-    from .common.settings import PACKAGE_NAME
     from .common.utils.environment import AfiEnvironmentCommand
     from .common.utils.changelog import AfiChangelogCommand
     from .common.utils.reloader import AfiReloadListener
@@ -18,12 +18,16 @@ if int(sublime.version()) > 3113:
     from .core.aliases import AfiCheckAliasesCommand
     from .core.themes import AfiPatchThemesCommand
 
+    from .common.settings import PACKAGE_NAME
+
+    NOPC_MSG = "It seems like you don't have Package Control installed"
+
     def init():
         settings.init()
         icons.init()
 
     def prepare():
-        start_msg = "{}: Preparing to configure the plugin. ".format(
+        start_msg = "Preparing to configure \"{}\". ".format(
             PACKAGE_NAME
         ) + "Do not close the Sublime Text."
 
@@ -47,19 +51,30 @@ if int(sublime.version()) > 3113:
         sublime.set_timeout_async(reload, 5000)
 
     def plugin_loaded():
-        from package_control import events
+        try:
+            from package_control import events
 
-        if events.post_upgrade(PACKAGE_NAME) or events.install(PACKAGE_NAME):
-            prepare()
-        else:
+            if events.post_upgrade(PACKAGE_NAME) or events.install(
+                PACKAGE_NAME
+            ):
+                prepare()
+            else:
+                init()
+        except Exception as error:
+            logging.log(NOPC_MSG)
+            logging.dump(error)
             init()
 
     def plugin_unloaded():
         settings.clear_listener()
 
-        from package_control import events
+        try:
+            from package_control import events
 
-        if events.pre_upgrade(PACKAGE_NAME) or events.remove(PACKAGE_NAME):
-            cleaning.clean_all()
+            if events.pre_upgrade(PACKAGE_NAME) or events.remove(PACKAGE_NAME):
+                cleaning.clean_all()
+        except Exception as error:
+            logging.log(NOPC_MSG)
+            logging.dump(error)
 else:
     raise ImportWarning("Doesn't support Sublime Text versions prior to 3114")
