cf3db8660114e03ca13f23d56ba1689e21751fe6
DeathAxe <deathaxe@googlemail.com>
2018-12-16 20:44:38 +0100

    Remove CHANGELOG

--- A File Icon.py
+++ A File Icon.py
@@ -9,7 +9,6 @@ if int(sublime.version()) >= 3114:
     from .core import aliases
 
     from .common.utils.environment import AfiEnvironmentCommand
-    from .common.utils.changelog import AfiChangelogCommand
     from .common.utils.reloader import AfiReloadListener
     from .common.utils.reloader import AfiReloadCommand
     from .common.utils.cleaning import AfiCleanCommand
